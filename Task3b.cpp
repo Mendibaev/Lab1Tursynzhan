456179914229a5b87be0bd40d9422d3319758501
Mendibaev <mendibaev98@gmail.com>
2016-09-09 14:32:29 +0600

    Add files via upload

---
+++ Task3b.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <cmath>
+using namespace std; 
+int main()
+{
+int a,b;
+cout <<"enter 2 numbers\n";
+cin >>a>>b;
+cout <<endl<<a+b;
+cout <<endl<<a*b;
+cout <<endl<<a-b;
+return 0;
+}
---
+++ Task3c.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+cout<<"1\t"<<"Abzal\t"<<"Aryn\t"<<"Armanovich";
+cout<<endl<<"2\t"<<"Muhtarhanov\t"<<"Daniyar\t"<<"Baurzhanovich";
+cout<<endl<<"3\t"<<"Armanov\t"<<"Kairat\t\t"<<"Maratovich";
+cout<<endl<<"4\t"<<"Mendibaev\t\t"<<"Tursynzhan\t\t"<<"Baurzhnovich";
+
+return 0;
+}
+
+
+
+
+
